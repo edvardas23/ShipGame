38b8a8add027286a27e1bf7b12000ff3be87455c
Amzond <94296972+Amzond@users.noreply.github.com>
2022-10-08 19:51:23 +0300

    Strategy pattern

---
+++ Game/MVVM/Model/ShotModels/ShootStrategy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient.MVVM.Model.ShotModels
+{
+    class ShootStrategy
+    {
+        public interface IStrategy
+        {
+            object Damage(object data);
+        }
+
+        class ClassicShot : IStrategy
+        {
+            public object Damage(object data)
+            {
+                return 1;
+            }
+        }
+        class FugueShoot : IStrategy
+        {
+            public object Damage(object data)
+            {
+                return 4;
+            }
+        }
+
+    }
+}
